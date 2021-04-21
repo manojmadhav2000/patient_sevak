f8b5b7ad7cb04de1d117858dd5519e1bca5ce663
manojmadhav2000 <mini.project20001@gmail.com>
2021-04-22 01:39:20 +0530

    Main and constant.dart

---
+++ lib/components/profilePhoto.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class ProfilePhoto extends StatelessWidget {
+  final BorderRadius borderRadius = BorderRadius.circular(50);
+  final Image image;
+
+  ProfilePhoto({
+    Key key,
+    @required this.image,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 20,
+      margin: EdgeInsets.all(20),
+      shape: RoundedRectangleBorder(
+        borderRadius: borderRadius,
+      ),
+      color: Colors.grey[400],
+      child: ClipRRect(borderRadius: borderRadius, child: image),
+    );
+  }
+}
---
+++ lib/modals/doctor.dart
@@ -0,0 +1,17 @@
+class Doctor {
+  String name, surname, about, profilePhoto, branch, workingTime;
+  int patientCount, experience;
+  double rating;
+
+  Doctor({
+    this.name,
+    this.surname,
+    this.about,
+    this.profilePhoto,
+    this.branch,
+    this.patientCount,
+    this.experience,
+    this.rating,
+    this.workingTime,
+  });
+}
